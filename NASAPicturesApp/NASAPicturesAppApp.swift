657e7f55f05d24f0950127c7f8095a7954dd7426
William Smith <WilliamSmith107@icloud.com>
2025-07-09 17:21:04 +0100

    Initial Commit

---
+++ NASAPicturesApp/NASAPicturesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAPicturesAppApp.swift
+//  NASAPicturesApp
+//
+//  Created by William Smith on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct NASAPicturesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
