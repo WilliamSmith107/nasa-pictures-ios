75b5b7ee1137e8b041eb33309e5102e0b19e39cf
William Smith <WilliamSmith107@icloud.com>
2025-07-09 17:32:10 +0100

    Added NASAPictures package.

---
+++ NASAPictures/Sources/AppFeature/NASAPictures.swift
@@ -0,0 +1,8 @@
+//
+//  NASAPictures.swift
+//  AppFeature
+//
+//  Created by William Smith on 09/07/2025.
+//
+
+import Foundation
---
+++ NASAPictures/Sources/AppFeature/NASAPicturesView.swift
@@ -0,0 +1,20 @@
+//
+//  NASAPicturesView.swift
+//  AppFeature
+//
+//  Created by William Smith on 09/07/2025.
+//
+
+import SwiftUI
+
+public struct NASAPicturesView: View {
+	public init() {}
+
+	public var body: some View {
+        Text("NASAPicturesView")
+    }
+}
+
+#Preview {
+	NASAPicturesView()
+}
